23090de5fa17fcf631d3481a86c9ff89e4dc99aa
Ignacio Vetrano <ivetrano@fi.uba.ar>
2025-06-22 19:39:33 -0300

    Creo una topologia de 3 switches con 4 hosts

--- src/topology.py
+++ src/topology.py
@@ -1,6 +1,6 @@
-from mininet . topo import Topo
+from mininet.topo import Topo
 
-class Topo (Topo):
+class TopoTP2 (Topo):
     def __init__ (self):
         # Initialize topology
         Topo.__init__(self)
@@ -8,15 +8,20 @@ class Topo (Topo):
         # Create switch
         s1 = self.addSwitch('switch_1')
         s2 = self.addSwitch('switch_2')
+        s3 = self.addSwitch('switch_3')
 
         # Create hosts
-        h1 = self . addHost ('host_1')
-        h2 = self . addHost ('host_2')
-        h3 = self . addHost ('host_3')
+        h1 = self.addHost ('host_1')
+        h2 = self.addHost ('host_2')
+        h3 = self.addHost ('host_3')
+        h4 = self.addHost ('host_4')
 
         # Add links between switches and hosts self . addLink ( s1 , s2 )
-        self . addLink ( s1 , h1 )
-        self . addLink ( s1 , h2 )
-        self . addLink ( s2 , h3 )
+        self.addLink(s1, h1)
+        self.addLink(s1, h2)
+        self.addLink(s3, h3)
+        self.addLink(s3, h4)
+        self.addLink(s2, s3)
+        self.addLink(s1, s2)
 
-topos = {'customTopo': Topo}
+topos = {'customTopo': TopoTP2}
