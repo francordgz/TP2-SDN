d379fa051cc5b3ad575aebee833d7db441c1430c
Ignacio Vetrano <ivetrano@fi.uba.ar>
2025-06-23 18:44:30 -0300

    Firewall funcionando para ipv4

--- src/firewall.py
+++ src/firewall.py
@@ -13,6 +13,7 @@ from pox.lib.revent import *
 from pox.lib.util import dpidToStr
 from pox.lib.addresses import EthAddr
 from collections import namedtuple
+import pox.lib.packet as pkt
 import os
 import json
 ''' Add your imports here ... '''
@@ -22,11 +23,22 @@ policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ['HOME']
 
 ''' Add your global variables here ... '''
 DPID_FIREWALL_SWITCH = 1
-
 NOMBRE_ARCHIVO_CONFIGURACION = os.path.join(
     os.path.dirname(__file__),
     "..", "config.json"
 )
+
+
+def obtener_dl_type(version_ip):
+    '''
+    Returns the dl_type based on the version of the protocol.
+    '''
+    if version_ip == "4":
+        return pkt.ethernet.IP_TYPE  # IPv4
+    elif version_ip == "6":
+        return pkt.ethernet.IPV6_TYPE  # IPv6
+    else:
+        return None
 
 
 def cargar_reglas(nombre_archivo):
@@ -35,11 +47,27 @@ def cargar_reglas(nombre_archivo):
     return config.get("reglas", [])
 
 
-def crear_regla_drop(dl_src=None, dl_dst=None):
+def obtener_protocolo_transporte(version_ip, protocolo_transporte):
+    if version_ip == "4" and protocolo_transporte == "TCP":
+        return pkt.ipv4.TCP_PROTOCOL
+    elif version_ip == "4" and protocolo_transporte == "UDP":
+        return pkt.ipv4.UDP_PROTOCOL
+    elif version_ip == "6" and protocolo_transporte == "TCP":
+        return pkt.ipv6.TCP_PROTOCOL
+    elif version_ip == "6" and protocolo_transporte == "UDP":
+        return pkt.ipv6.UDP_PROTOCOL
+    else:
+        return None
+
+
+def crear_regla_drop(dl_src=None, dl_dst=None, version_ip=None, protocolo_transporte=None, dst_port=None):
     log.debug("Creo regla de drop para: dl_src=%s, dl_dst=%s", dl_src, dl_dst)
     msg = of.ofp_flow_mod()
     msg.match.dl_src = EthAddr(dl_src) if dl_src else None
     msg.match.dl_dst = EthAddr(dl_dst) if dl_dst else None
+    msg.match.dl_type = obtener_dl_type(version_ip) if version_ip else None
+    msg.match.nw_proto = obtener_protocolo_transporte(version_ip, protocolo_transporte) if protocolo_transporte else None
+    msg.match.tp_dst = int(dst_port) if dst_port else None
     msg.actions = []  # Drop
     return msg
 
@@ -56,7 +84,11 @@ class Firewall(EventMixin):
             log.info("Instalando reglas de bloqueo en s" + str(DPID_FIREWALL_SWITCH))
 
             for regla in cargar_reglas(NOMBRE_ARCHIVO_CONFIGURACION):
-                msg = crear_regla_drop(dl_src=regla.get("dl_src"), dl_dst=regla.get("dl_dst"))
+                msg = crear_regla_drop(dl_src=regla.get("src_mac"),
+                                       dl_dst=regla.get("dst_mac"),
+                                       version_ip=regla.get("ip_version"),
+                                       protocolo_transporte=regla.get("transport_protocol"),
+                                       dst_port=regla.get("dst_port"))
                 event.connection.send(msg)
 
 
